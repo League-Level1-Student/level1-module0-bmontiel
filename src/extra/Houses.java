8ab6587741ccd1ef97f168f821e2e16120c5c868
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-19 10:52:55 -0800

    Houses

--- src/extra/Houses.java
+++ src/extra/Houses.java
@@ -6,7 +6,13 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 public static void main(String[] args) {
-drawHouse();
+	
+for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			drawHouse();
+		}
+	
+}
 	
 }
 static void drawHouse() {
@@ -14,14 +20,15 @@ static void drawHouse() {
 	nancy.setX(50);
 	nancy.setY(500);
 	nancy.penDown();
+	nancy.setSpeed(20);
+	nancy.
 	nancy.setPenColor(Color.pink);
 	nancy.move(100);
-	nancy.turn(30);
+	nancy.turn(90);
 	nancy.move(50);
-	nancy.turn(120);
-	nancy.move(50);
-	nancy.turn(30);
+	nancy.turn(90);
 	nancy.move(100);
+	
 	nancy.setPenColor(Color.GREEN);
 	nancy.turn(270);
 	nancy.move(20);
