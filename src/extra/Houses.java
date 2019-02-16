4a1feebb67307137e9ef56e9463181a1d78b6c8e
ilabstaff <ilabstaff@172.16.116.71>
2019-02-16 10:59:18 -0800

    Houses, IntroToMethods

--- src/extra/Houses.java
+++ src/extra/Houses.java
@@ -10,6 +10,7 @@ public static void main(String[] args) {
 	nancy.setX(50);
 	nancy.setY(500);
 	Robot.setWindowColor(Color.BLACK);
+	
 for (int i = 0; i < 3; i++) {
 			drawHouse("medium");
 			drawHouse("small");
@@ -26,21 +27,37 @@ static void drawHouse(String height) {
 	}
 	if (height.equals("big")) {
 		distance=250;
+		
 	}
 	
 	nancy.penDown();
-	nancy.setSpeed(20);
+	nancy.setSpeed(50);
 	nancy.setRandomPenColor();
 	nancy.move(distance);
-	nancy.turn(90);
-	nancy.move(50);
-	nancy.turn(90);
+if (distance==250) {
+		drawFlatRoof();
+	}
+else {
+	drawPointyRoof();
+}
 	nancy.move(distance);
 	nancy.setPenColor(Color.GREEN);
 	nancy.turn(270);
 	nancy.move(20);
 	nancy.turn(270);
 	
+}
+static void drawPointyRoof() {
+	nancy.turn(45);
+	nancy.move(40);
+	nancy.turn(90);
+	nancy.move(40);
+	nancy.turn(45);
+}
+static void drawFlatRoof() {
+	nancy.turn(90);
+	nancy.move(50);
+	nancy.turn(90);
 }
 
 
---
+++ src/extra/IntroToMethods.java
@@ -0,0 +1,18 @@
+package extra;
+
+public class IntroToMethods {
+public static void main(String[] args) {
+	
+	WeRock();
+	
+	
+	
+
+}
+static void WeRock() {
+	System.out.println("The League rocks");
+}
+static void rocker(String name) {
+	
+}
+}
