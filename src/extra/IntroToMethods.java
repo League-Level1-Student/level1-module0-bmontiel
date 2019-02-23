ef24cff9f6921ec55ed3c64504976a128eb1624d
ilabstaff <ilabstaff@172.16.116.71>
2019-02-23 10:57:09 -0800

    IntoToMethods

--- src/extra/IntroToMethods.java
+++ src/extra/IntroToMethods.java
@@ -4,15 +4,42 @@ public class IntroToMethods {
 public static void main(String[] args) {
 	
 	WeRock();
-	
-	
-	
-
+	rocker("Bob");
+	truth("nancy");
+	echo("hello");
+	repeat("mayday", 4);
+	theBest("theBest");
+		int sum= add(2,2);
+	System.out.println(sum);
 }
 static void WeRock() {
 	System.out.println("The League rocks");
 }
 static void rocker(String name) {
-	
+	System.out.println(name +" rocks");
+}
+static void truth(String name){
+	if (name.equals("Nancy")) {
+		System.out.println(name +" is awesome");
+	}
+	else {
+		System.out.println(name +" is annoying");
+	}
+}
+static void echo(String word) {
+	for (int i = 0; i < 2; i++) {
+		System.out.println(word);
+	}	
+}
+static void repeat(String word, int times) {
+	for (int i = 0; i < times; i++) {
+		System.out.println(word);
+	}
+}
+static void theBest(String Best){
+	System.out.println("theBest");
+}
+static void add(int a, int b){
+	return a+b;
 }
 }
